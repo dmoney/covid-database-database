c6c9eee0a46121cff8c77d86fd029237be9a727e
Dustin King <cathodion@gmail.com>
2020-06-26 16:27:32 -0400

    add Source, Field, and Scope models

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin.site.register(models.Source)
+admin.site.register(models.Field)
+admin.site.register(models.Scope)
--- server/core/models.py
+++ server/core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Source(models.Model):
+    name=models.CharField(max_length=100)
+    url=models.URLField()
+    description=models.TextField()
+    author_name=models.CharField(max_length=100)
+    author_url=models.URLField()
+
+    def __str__(self):
+        return f"{self.name} (by {self.author_name or 'Unknown'})"
+
+class Field(models.Model):
+    name=models.CharField(max_length=100)
+    type=models.CharField(max_length=100)
+    description=models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Scope(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+
+    def __str__(self):
+        return self.name
