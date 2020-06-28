5d7d4cb77912c9e7c023bfc143e0e36dc8c18aec
Dustin King <cathodion@gmail.com>
2020-06-28 14:13:06 -0400

    make source.description field markdown

--- server/core/views.py
+++ server/core/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 from .models import *
 
+import markdown
+
 # Create your views here.
 
 def home(request):
@@ -15,8 +17,15 @@ def source(request, id):
     source_obj = get_object_or_404(Source, pk=id)
     using = source_obj.using.all()
     used_by = source_obj.used_by.all()
+
+    # cuz who needs functions when we can instantiate a class
+    # to then call a method upon...
+    md = markdown.Markdown()
+    description_html = md.convert(source_obj.description)
+
     return render(request, 'source.html', {
         "source": source_obj,
+        "description_html": description_html,
         "using_sources": using,
         "used_by_sources": used_by,
         })
