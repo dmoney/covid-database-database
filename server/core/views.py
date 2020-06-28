1a815fa51489019d26f6a0745ae7269c83d60b7d
Dustin King <cathodion@gmail.com>
2020-06-27 23:57:02 -0400

    add some view functions

--- server/core/views.py
+++ server/core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+from .models import *
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('Why not go to <a href="/sources">sources</a>')
+
+def sources(request):
+    return HttpResponse("<h2>Sources</h2>" + "<br/>".join([f"<a href='/source/{x.id}'>{str(x)}</a>" for x in Source.objects.all()]))
+
+def source(request, id):
+    source_obj = get_object_or_404(Source, pk=id)
+    # source_obj = Source.create(name="abaldkjflsadkfj")
+    return HttpResponse(f"<h2>{source_obj.name}</h2>")
--- server/cvdbdb/urls.py
+++ server/cvdbdb/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", home),
+    path('sources/', sources),
+    path('source/<int:id>', source),
 ]
